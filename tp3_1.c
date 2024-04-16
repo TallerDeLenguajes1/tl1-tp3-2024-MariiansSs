82e010fa909bf88d832064757b81adf40318face
Mariano <marianogotardo@gmail.com>
2024-04-16 12:11:57 -0300

    Punto 3)c)

--- tp3_1.c
+++ tp3_1.c
@@ -5,12 +5,23 @@
 #define C 12
 #define F 5
 
+void cargarValores(int matriz[F][C]);
+void promedioGanancias(int matriz[F][C]);
+
+
 int main()
 {
     srand(time(NULL));
 
     int matriz[F][C];
+    cargarValores(matriz);
+    promedioGanancias(matriz);
+    
+    return 0;
+}
 
+void cargarValores(int matriz[F][C])
+{
     for (int i = 0; i < F; i++)
     {
         for (int j = 0; j < C; j++)
@@ -21,5 +32,21 @@ int main()
         printf("\n");
     }
     
-    return 0;
+}
+
+void promedioGanancias (int matriz[F][C])
+{
+    int gananciasT = 0;
+    int promedioG;
+    for (int i = 0; i < F; i++)
+    {
+        for (int j = 0; j < 1; j++)
+        {
+             gananciasT = gananciasT + matriz[i][j];
+        }
+    }
+    promedioG = gananciasT/5;
+    
+    printf("La cantidad de ganancias por año es: %d", gananciasT);
+    printf("\nLa cantidad de ganancia promedio por año es: %d", promedioG);
 }
